8c1993221864fe1622aae405bb96eaac55ac252b
worry <2428405332@qq.com>
2025-05-19 23:42:24 +0800

    feat: 添加 ThoughtChain 组件及其样式

---
+++ packages/element-x-ui/src/components/ThoughtChain/main.vue
@@ -0,0 +1,220 @@
+<template>
+  <div class="el-x-thoughtchain">
+    <el-timeline ref="timelineRef" :style="{
+      maxWidth: maxWidth,
+    }">
+      <transition-group name="thought-chain" tag="div">
+        <el-timeline-item v-for="item in thinkingItems" :key="getId(item)" :type="getType(item)"
+          :timestamp="getTitle(item)" :hide-timestamp="item.hideTitle" :placement="item.placement || 'top'">
+          <div v-if="!item.isCanExpand">
+            <typewriter :content="getThinkTitle(item)" :is-markdown="item.isMarkdown" :typing="item.typing" />
+          </div>
+          <el-collapse v-else-if="!item.isDefaultExpand" @change="() => handleExpand(item)">
+            <el-collapse-item :title="getThinkTitle(item)">
+              <typewriter :content="getThinkContent(item)" :is-markdown="item.isMarkdown" :typing="item.typing" />
+            </el-collapse-item>
+          </el-collapse>
+          <el-collapse v-else v-model="defaultActiveNodes" @change="() => handleExpand(item)">
+            <el-collapse-item :title="getThinkTitle(item)" :name="String(getId(item))">
+              <typewriter :content="getThinkContent(item)" :is-markdown="item.isMarkdown" :typing="item.typing" />
+            </el-collapse-item>
+          </el-collapse>
+
+          <template slot="dot">
+            <div class="el-x-thoughtchain-item-dot" :style="{ margin: dotMargin }">
+              <slot name="icon" :item="item">
+                <el-button circle :type="getType(item)" :loading="isLoading(item)" :size="dotSize">
+                  <template slot="loading">
+                    <i class="el-icon-loading el-x-thoughtchain-loading"></i>
+                  </template>
+
+                  <i v-if="!isLoading(item) && !isError(item)" class="el-icon-check"></i>
+                  <i v-if="!isLoading(item) && isError(item)" class="el-icon-close"></i>
+                </el-button>
+              </slot>
+            </div>
+          </template>
+        </el-timeline-item>
+      </transition-group>
+    </el-timeline>
+  </div>
+</template>
+
+<script>
+import { get } from 'lodash';
+import Typewriter from '../Typewriter/main.vue';
+import '../../styles/ThoughtChain.scss';
+
+export default {
+  name: "ElXThoughtChain",
+  components: {
+    Typewriter
+  },
+  props: {
+    thinkingItems: {
+      type: Array,
+      default: () => []
+    },
+    dotSize: {
+      type: String,
+      default: 'mini'
+    },
+    maxWidth: {
+      type: String,
+      default: '600px'
+    },
+    lineGradient: {
+      type: Boolean,
+      default: false
+    },
+    rowKey: {
+      type: String,
+      default: 'id'
+    },
+    statusKey: {
+      type: String,
+      default: 'status'
+    },
+    statusEnum: {
+      type: Object,
+      default: () => ({
+        loading: {
+          value: 'loading',
+          type: 'warning',
+        },
+        error: {
+          value: 'error',
+          type: 'danger',
+        },
+        success: {
+          value: 'success',
+          type: 'success',
+        },
+      })
+    },
+    titleKey: {
+      type: String,
+      default: 'title'
+    },
+    thinkTitleKey: {
+      type: String,
+      default: 'thinkTitle'
+    },
+    thinkContentKey: {
+      type: String,
+      default: 'thinkContent'
+    }
+  },
+  data() {
+    return {
+      colorArr: {
+        info: 'var(--el-color-primary)',
+        success: 'var(--el-color-success)',
+        warning: 'var(--el-color-warning)',
+        danger: 'var(--el-color-danger)',
+        primary: 'var(--el-color-primary)',
+      },
+      defaultActiveNodes: []
+    };
+  },
+  computed: {
+    dotMargin() {
+      switch (this.dotSize) {
+        case 'small':
+          return '-4px 0 0 -7px';
+        case 'large':
+          return '-11px 0 0 -15px';
+        default:
+          return '-8px 0 0 -11px';
+      }
+    },
+    getLineColor() {
+      if (this.thinkingItems.length) {
+        const arr = this.thinkingItems.map((item) => {
+          const _type_ = this.getType(item);
+          if (_type_) {
+            return this.colorArr[_type_];
+          }
+          return '';
+        });
+        return arr;
+      }
+      return [];
+    },
+    activeNamesComputed() {
+      return this.thinkingItems
+        .filter(item => item.isCanExpand && item.isDefaultExpand)
+        .map(item => String(this.getId(item)));
+    }
+  },
+  watch: {
+    activeNamesComputed: {
+      handler(v) {
+        this.defaultActiveNodes = [...v];
+      },
+      immediate: true
+    },
+    getLineColor() {
+      this.$nextTick(() => {
+        this.getEle();
+      });
+    }
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this.getEle();
+    });
+  },
+  methods: {
+    handleExpand(item) {
+      this.$emit('handleExpand', item);
+    },
+    setRadialGradient(colors, ele) {
+      const length = ele.length;
+      Array.from(ele).forEach((item, index) => {
+        const line = item.children[0];
+        if (line) {
+          line.setAttribute('style', `
+            border: none;
+            width:2px;
+            background: linear-gradient(to bottom, ${colors[index]} 0% , ${colors[index < length ? index + 1 : index]} 100%);
+          `);
+        }
+      });
+    },
+    getEle() {
+      if (this.getLineColor && this.$refs.timelineRef && this.lineGradient) {
+        const ele = this.$refs.timelineRef.$el.children[0].children;
+        this.setRadialGradient(this.getLineColor, ele);
+      }
+    },
+    isLoading(item) {
+      const status = this.getStatus(item);
+      return status === this.statusEnum.loading.value;
+    },
+    isError(item) {
+      const status = this.getStatus(item);
+      return status === this.statusEnum.error.value;
+    },
+    getId(item) {
+      return get(item, this.rowKey);
+    },
+    getType(item) {
+      const status = this.getStatus(item);
+      return (this.statusEnum[status] && this.statusEnum[status].type) || 'success';
+    },
+    getTitle(item) {
+      return get(item, this.titleKey) || '';
+    },
+    getThinkTitle(item) {
+      return get(item, this.thinkTitleKey) || '';
+    },
+    getThinkContent(item) {
+      return get(item, this.thinkContentKey) || '';
+    },
+    getStatus(item) {
+      return get(item, this.statusKey);
+    }
+  }
+};
+</script>
--- packages/element-x-ui/src/index.js
+++ packages/element-x-ui/src/index.js
@@ -3,32 +3,31 @@ import ElXBubble from './components/Bubble/main.vue'
 import ElXBubbleList from './components/BubbleList/main.vue'
 import ElXWelcome from './components/Welcome/main.vue'
 import ElXPrompts from './components/Prompts/main.vue'
-import ElXConversations from './components/Conversations/main.vue';
+import ElXConversations from './components/Conversations/main.vue'
 import ElXThinking from './components/Thinking/main.vue'
 import ElXThink from './components/Think/main.vue'
-
-
+import ElThoughtChain from './components/ThoughtChain/main.vue'
 
 const components = [
-  ElXTypewriter,
-  ElXBubble,
-  ElXBubbleList,
-  ElXWelcome,
-  ElXPrompts,
-  ElXConversations,
-  ElXThinking,
-  ElXThink
+	ElXTypewriter,
+	ElXBubble,
+	ElXBubbleList,
+	ElXWelcome,
+	ElXPrompts,
+	ElXConversations,
+	ElXThinking,
+	ElXThink,
+	ElThoughtChain
 ]
 
 const install = function (Vue) {
-  components.forEach(component => {
-    Vue.component(component.name, component)
-  })
-
+	components.forEach((component) => {
+		Vue.component(component.name, component)
+	})
 }
 
 export default {
-  version: '1.0.0',
-  install,
-  ...components
+	version: '1.0.0',
+	install,
+	...components
 }
