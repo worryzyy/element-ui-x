a2d7e21a43f59428476550e780ded409dc4c2cb5
weilei <wlworry@163.com>
2025-05-29 16:58:39 +0800

    chore(examples): 更新 vue-element-ui-x 依赖版本为 ^0.1.7-beta

--- examples/src/main.js
+++ examples/src/main.js
@@ -1,12 +1,11 @@
-import Vue from 'vue';
 import ElementUI from 'element-ui';
-import './assets/styles/reset.css';
 import 'element-ui/lib/theme-chalk/index.css';
+import Vue from 'vue';
+import './assets/styles/reset.css';
 
 import ElementUIX from 'vue-element-ui-x';
 import App from './App.vue';
 import router from './router';
-
 Vue.use(ElementUI);
 Vue.use(ElementUIX);
 
